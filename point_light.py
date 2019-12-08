a1354abc1dec8c7effabc81e140ee93686756aca
howetuft <howetuft@gmail.com>
2019-12-08 18:17:44 +0100

    Add a Rendering Power property on point lights
    A Power property will be needed for physical based renderers (Luxcore,
    Blender, Appleseed...)

--- point_light.py
+++ point_light.py
@@ -5,12 +5,14 @@ from pivy import coin
 import light
 from arch_texture_utils.resource_utils import iconPath
 
+
 class PointLight(light.Light):
-    INITIAL_RADIUS = 50.0
+    DEFAULT_RADIUS = 50.0
+    DEFAULT_POWER = 60.0
 
     def __init__(self, obj):
         super().__init__(obj)
-    
+
     def setProperties(self, obj):
         super().setProperties(obj)
 
@@ -21,14 +23,19 @@ class PointLight(light.Light):
                             "The position of the light in the scene.").Location = FreeCAD.Vector(0, -1, 0)
 
         if not 'Radius' in pl:
-            obj.addProperty("App::PropertyLength", "Radius", "PointLight",
-                            "The radius of the point light representation.").Radius = PointLight.INITIAL_RADIUS
+            obj.addProperty("App::PropertyLength", "Radius", "PointLight","The radius of the point light representation.")
+            obj.Radius = PointLight.DEFAULT_RADIUS
+
+        if not 'RenderingPower' in pl:
+            obj.addProperty("App::PropertyFloat", "RenderingPower", "Light", "The power of the light for rendering.") # TODO Should be App::PropertyPower when this type is created
+            obj.RenderingPower = PointLight.DEFAULT_POWER
+
         self.type = 'PointLight'
-    
+
 class ViewProviderPointLight(light.ViewProviderLight):
     def __init__(self, vobj):
         super().__init__(vobj)
-    
+
     def attach(self, vobj):
         super().attach(vobj)
 
@@ -39,7 +46,7 @@ class ViewProviderPointLight(light.ViewProviderLight):
 
     def createGeometry(self):
         sphere = coin.SoSphere()
-        sphere.radius.setValue(PointLight.INITIAL_RADIUS)
+        sphere.radius.setValue(PointLight.DEFAULT_RADIUS)
         return sphere
 
     def updateRadius(self):
@@ -54,7 +61,7 @@ class ViewProviderPointLight(light.ViewProviderLight):
 
     def getIcon(self):
         return iconPath("PointLight.svg")
-    
+
 def createPointLight():
     obj = FreeCAD.ActiveDocument.addObject("App::FeaturePython", "PointLight")
     light = PointLight(obj)
